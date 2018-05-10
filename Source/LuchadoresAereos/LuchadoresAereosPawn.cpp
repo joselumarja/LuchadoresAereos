8584888c29eb88ecb675409fe5ace38fc14fefad
joselumarja <34023329+joselumarja@users.noreply.github.com>
2018-05-10 13:20:02 +0200

    M

--- Source/LuchadoresAereos/LuchadoresAereosPawn.cpp
+++ Source/LuchadoresAereos/LuchadoresAereosPawn.cpp
@@ -30,20 +30,20 @@ ALuchadoresAereosPawn::ALuchadoresAereosPawn()
 	// Cache our sound effect
 	static ConstructorHelpers::FObjectFinder<USoundBase> FireAudio(TEXT("/Game/TwinStick/Audio/TwinStickFire.TwinStickFire"));
 	FireSound = FireAudio.Object;
-
+	
 	// Create a camera boom...
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
 	CameraBoom->bAbsoluteRotation = true; // Don't want arm to rotate when ship does
 	CameraBoom->TargetArmLength = 1200.f;
-	CameraBoom->RelativeRotation = FRotator(-180.f, 0.f, 0.f);
+	CameraBoom->RelativeRotation = FRotator(-80.f, 0.f, 0.f);
 	CameraBoom->bDoCollisionTest = false; // Don't want to pull camera in when it collides with level
 
 	// Create a camera...
 	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("TopDownCamera"));
 	CameraComponent->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
 	CameraComponent->bUsePawnControlRotation = false;	// Camera does not rotate relative to arm
-
+	
 	// Movement
 	MoveSpeed = 1000.0f;
 	// Weapon
