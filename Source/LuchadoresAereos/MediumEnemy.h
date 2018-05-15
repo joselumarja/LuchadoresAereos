830389c687e4f49e5189891af491ad7fb11f075a
Fabioballest <fabio.ballesteros@alu.uclm.es>
2018-05-15 13:39:47 +0200

    Gerarkia de henemijos kreada

---
+++ Source/LuchadoresAereos/MediumEnemy.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy.h"
+#include "MediumEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUCHADORESAEREOS_API AMediumEnemy : public AEnemy
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/LuchadoresAereos/TankEnemy.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy.h"
+#include "TankEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUCHADORESAEREOS_API ATankEnemy : public AEnemy
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
