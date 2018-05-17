2c62ca8259f9948f5faee91f54b8b7bb6f57b62e
joselumarja <34023329+joselumarja@users.noreply.github.com>
2018-05-17 14:18:57 +0200

    Esqueleto State

---
+++ Source/LuchadoresAereos/DodgeState.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DodgeState.h"
+#include "Enemy.h"
+
+void UDodgeState::Enter(IEnemyState& From, AEnemy& Enemy)
+{
+
+}
+
+void UDodgeState::Update(AEnemy& Enemy)
+{
+
+}
+
+
---
+++ Source/LuchadoresAereos/DodgeState.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "EnemyState.h"
+#include "DodgeState.generated.h"
+
+class AEnemy;
+
+UCLASS()
+class LUCHADORESAEREOS_API UDodgeState : public UObject, public IEnemyState
+{
+	GENERATED_BODY()
+	
+public:
+
+	void Enter(IEnemyState& From, AEnemy& Enemy) override;
+	void Update(AEnemy& Enemy) override;
+	
+	
+};
